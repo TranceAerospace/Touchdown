a9995f12ce79e6c61dca40f885f7e03c7a8f8da4
Trance <xp46290@gmail.com>
2023-04-12 06:59:25 -0400

    Assets and initial  project set up

---
+++ Touchdown/Utility/Constant.swift
@@ -0,0 +1,21 @@
+//
+//  Constant.swift
+//  Touchdown
+//
+//  Created by Mark Trance on 4/12/23.
+//
+
+import SwiftUI
+
+// Data
+// Color
+let colorBackground = Color("ColorBackground")
+let colorGray: Color = Color(UIColor.systemGray4)
+// Layout
+// UX
+// API
+// Image
+// Font
+// String
+// MISC
+
