8135d8293e200b635d155b12affb2c928face066
Trance <xp46290@gmail.com>
2023-04-13 07:19:25 -0400

    Modified content view to implement custom nav and footer views

--- Touchdown/App/ContentView.swift
+++ Touchdown/App/ContentView.swift
@@ -8,17 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    // MARK: - Properties
+    
+    // MARK: - Body
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        ZStack {
+            VStack(spacing: 0) {
+                NavigationBarView()
+                    .padding(.horizontal, 15)
+                    .padding(.bottom)
+                // TODO: - Find iOS 16 version of handling the notch
+                    .padding(.top, UIApplication.shared.windows.first?.safeAreaInsets.top)
+                    .background(Color.white)
+                    .shadow(color: Color.black.opacity(0.05), radius: 5, x: 0, y: 5)
+                Spacer()
+                
+                FooterView()
+                    .padding(.horizontal)
+                
+            } //: VStack
+            .background(colorBackground.ignoresSafeArea(.all, edges: .all))
+        } //: ZStack
+        .ignoresSafeArea(.all, edges: .top)
     }
 }
 
+// MARK: - Preview
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
