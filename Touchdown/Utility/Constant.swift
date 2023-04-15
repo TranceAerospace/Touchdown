78774e3c36dfce8f86e4a35eae8223f94381367b
Trance <xp46290@gmail.com>
2023-04-15 12:25:44 -0400

    Update constants to contain values for the new grid layout

--- Touchdown/Utility/Constant.swift
+++ Touchdown/Utility/Constant.swift
@@ -7,14 +7,23 @@
 
 import SwiftUI
 
+// TODO: - Make all of these enums instead of global
+
 // Data
 let players: [Player] = Bundle.main.decode("player.json")
+let categories: [Category] = Bundle.main.decode("category.json")
 
 // Color
 let colorBackground = Color("ColorBackground")
 let colorGray: Color = Color(UIColor.systemGray4)
 
 // Layout
+let columnSpacing: CGFloat = 10
+let rowSpacing: CGFloat = 10
+var gridLayout: [GridItem] {
+    return Array(repeating: GridItem(.flexible(), spacing: rowSpacing), count: 2)
+}
+
 // UX
 // API
 // Image
