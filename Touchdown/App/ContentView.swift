7d7d0040b89b49d1c0fc2d892fda6dffd094b6d7
Trance <xp46290@gmail.com>
2023-04-15 13:14:18 -0400

    Implement brand view into main screen

--- Touchdown/App/ContentView.swift
+++ Touchdown/App/ContentView.swift
@@ -40,6 +40,10 @@ struct ContentView: View {
                         } //: VGrid
                         .padding(15)
                         
+                        TitleView(title: "Brands")
+                        
+                        BrandGridView()
+                        
                         FooterView()
                             .padding(.horizontal)
                     } //: VStack
