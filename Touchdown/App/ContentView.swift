7d26a8d02e17ad96e4a0d84b51f7877d24a0220b
Trance <xp46290@gmail.com>
2023-04-15 12:49:39 -0400

    Add new product view to main screen

--- Touchdown/App/ContentView.swift
+++ Touchdown/App/ContentView.swift
@@ -31,6 +31,15 @@ struct ContentView: View {
                         
                         CategoryGridView()
                         
+                        TitleView(title: "Helmets")
+                        
+                        LazyVGrid(columns: gridLayout, spacing: 15) {
+                            ForEach(products) { product in
+                                ProductItemView(product: product)
+                            } //: ForEach
+                        } //: VGrid
+                        .padding(15)
+                        
                         FooterView()
                             .padding(.horizontal)
                     } //: VStack
